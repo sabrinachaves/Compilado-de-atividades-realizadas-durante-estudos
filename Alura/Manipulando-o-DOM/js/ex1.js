cdee12094ddb9254092d2a5b129a2ed1319863e4
sabrinachs <sabrinach08@yahoo.com.br>
2022-06-19 20:59:16 -0300

    Manipulando o DOM

---
+++ Alura/Manipulando-o-DOM/js/ex1.js
@@ -0,0 +1,3 @@
+let texto =  document.getElementById('texto');
+
+texto.innerText = "O inverno já chegou!"
---
+++ Alura/Manipulando-o-DOM/js/ex2.js
@@ -0,0 +1,7 @@
+
+let botao = document.querySelector('#calcular');
+let resultado = document.querySelector('.resultado');
+
+botao.addEventListener("click", () =>{
+   resultado.innerHTML = "Fui clicado!";
+})
