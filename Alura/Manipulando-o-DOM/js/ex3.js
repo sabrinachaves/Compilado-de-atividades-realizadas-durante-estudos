4595f3416557134efd55f78df206d8abfddc1ee4
sabrinachs <sabrinach08@yahoo.com.br>
2022-06-20 08:32:10 -0300

    Manipulando estatisticas do robô

---
+++ Alura/Manipulando-o-DOM/js/ex3.js
@@ -0,0 +1,10 @@
+let lista = document.querySelector('.lista');
+lista.style.display = 'none';
+
+function mostrarTintas() {
+    lista.style.display = 'block';
+}
+
+function ocultarTintas() {
+    lista.style.display = 'none';
+}
