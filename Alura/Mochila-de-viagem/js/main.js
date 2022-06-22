9a8c0b668b5d779d09064f4e67fe03e3bca4ad24
sabrinachs <sabrinach08@yahoo.com.br>
2022-06-22 08:58:05 -0300

    Implementando local storage

--- Alura/Mochila-de-viagem/js/main.js
+++ Alura/Mochila-de-viagem/js/main.js
@@ -1,23 +1,55 @@
 const form = document.getElementById('novoItem');
 const lista = document.getElementById('lista');
+const itens = JSON.parse(localStorage.getItem("itens")) || [];
+
+itens.forEach((elemento) =>{
+    criaElemento(elemento)
+})
 
 form.addEventListener("submit", (e) =>{
     e.preventDefault();
 
-    const nome = e.target.elements['nome'].value;
-    const quantidade = e.target.elements['quantidade'].value;
-    criaElemento(nome, quantidade);
+    const nome = e.target.elements['nome'];
+    const quantidade = e.target.elements['quantidade'];
+
+    const existe = itens.find(elemento => nome === nome.value)
+
+    const itemAtual ={
+        "nome": nome.value,
+        "quantidade": quantidade.value
+    }
+
+    if(existe){
+        itemAtual.id = existe.id;
+        atualizaElemento(itemAtual);
+        itens[existe.id] = itemAtual;
+    }else{
+        itemAtual.id = itens.length;
+        criaElemento(itemAtual);
+        itens.push(itemAtual);
+    }
+
+
+    localStorage.setItem("itens", JSON.stringify(itens));
+
+    nome.value = "";
+    quantidade.value = "";
 })
 
-function criaElemento(nome, quantidade){
+function criaElemento(item){
     const novoItem = document.createElement('li');
     novoItem.classList.add('item');
 
     const numeroItem = document.createElement('strong');
-    numeroItem.innerHTML = quantidade;
+    numeroItem.innerHTML = item.quantidade;
 
     novoItem.appendChild(numeroItem);
-    novoItem.innerHTML += nome;
+    novoItem.dataset.id = item.id;
+    novoItem.innerHTML += item.nome;
 
     lista.appendChild(novoItem);
+}
+
+function atualizaElemento(item){
+   document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade;
 }
