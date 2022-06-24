da86b2d44e084b4f5f95693c26044491c28d459f
sabrinachs <sabrinach08@yahoo.com.br>
2022-06-24 17:57:54 -0300

    Dark mode

--- Alura/Manipulando-o-DOM/js/ex1.js
+++ Alura/Manipulando-o-DOM/js/ex1.js
@@ -1,3 +1,8 @@
 let texto =  document.getElementById('texto');
+let botaoDark = document.getElementById('btn');
 
 texto.innerText = "O inverno já chegou!"
+
+botaoDark.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode")
+})
