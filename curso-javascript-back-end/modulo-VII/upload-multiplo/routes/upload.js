12a07376ed99a592981988f9b38e02e78ca5fe38
sabrinachaves <sabrinach08@gmail.com>
2022-09-29 23:23:58 -0300

    modularização

--- curso-javascript-back-end/modulo-VII/upload-multiplo/routes/upload.js
+++ curso-javascript-back-end/modulo-VII/upload-multiplo/routes/upload.js
@@ -3,11 +3,8 @@ const router = express.Router(); //construtor de rotas do contexto do express
 const { index, success, error, upload } = require("../controllers/upload");
 
 router.get("/", index);
-
 router.post("/upload", upload);
-
 router.get("/success", success);
-
 router.get("/error", error);
 
 module.exports = router;
