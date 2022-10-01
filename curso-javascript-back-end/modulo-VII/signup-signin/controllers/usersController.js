a5c676ff36794f3c7ee39fd8631c2097c29484e0
sabrinachaves <sabrinach08@gmail.com>
2022-10-01 19:22:37 -0300

    Hash, salt e configuração do findOne

--- curso-javascript-back-end/modulo-VII/signup-signin/controllers/usersController.js
+++ curso-javascript-back-end/modulo-VII/signup-signin/controllers/usersController.js
@@ -1,18 +1,35 @@
 const dbConn = require("../infra/dbConn");
 const UserDAO = require("../dao/userDAO")(dbConn);
+const crypto = require("crypto");
 
 exports.signin = (req, res) => {
-  UserDAO.findOne((err, data) => {
-    if(err){
-      res.json({ message: err });
-    }else{
-      res.json(data); 
-    }
-  });
-};
+    UserDAO.findOne(req.body, (err, data, pwdIsValid) => {
+        if (data && pwdIsValid) {
+          res.status(200).json({ message: "Logged in successfully" });
+        }
+    
+        if (data && !pwdIsValid) {
+          res.status(400).json({ message: "User or password is wrong" });
+        }
+    
+        if (data === undefined) {
+          res.status(404).json({ message: "User not found" });
+        }
+    
+        if (err) {
+          res.status(500).json({ message: "Server unavailable" });
+        }
+      });
+    };
 
 exports.signup = (req, res) => {
-  UserDAO.save();
+  UserDAO.save(req.body, (err) => {
+    if(err) {
+        res.status(500).json({ message: `Falhou ao adicionar o usuário!` });
+    } else{
+        res.status(201).json({ message: "Usuário adicionado com sucesso!" })
+    }
+  });
 };
 
 // module.exports = {
--- curso-javascript-back-end/modulo-VII/signup-signin/dao/userDAO.js
+++ curso-javascript-back-end/modulo-VII/signup-signin/dao/userDAO.js
@@ -1,22 +1,42 @@
-class UserDAO{
-    constructor(dbConn) {
-        this.db = dbConn;
-    }
+const crypto = require("crypto");
+const { ulid } = require("ulid"); //gerador de ids
 
-    findOne(cb){
-        const sql = `SELECT * FROM users`
-        this.db.all(sql, cb);
-    }
+class UserDAO {
+  constructor(dbConn) {
+    this.db = dbConn;
+  }
 
-    save(){
-        console.log("saving user data...");
-    }
+  findOne(data, cb) {
+    const { email, password } = data;
+    const sql = `SELECT * FROM users WHERE email = ?;`;
+
+    this.db.get(sql, [email], (err, data) => {
+      if (data) {
+        const { salt, hash, id } = data;
+        const recalcHash = crypto
+          .pbkdf2Sync(password, salt, 1000, 64, "sha512")
+          .toString("hex");
+        cb(err, id, hash === recalcHash);
+      } else {
+        cb(err, data, false);
+      }
+    });
+  }
+
+  save(data, cb) {
+    const { name, email, password } = data;
+    const salt = crypto.randomBytes(16).toString("hex");
+    const hash = crypto
+      .pbkdf2Sync(password, salt, 1000, 64, "sha512")
+      .toString("hex");
+    const sql = `INSERT INTO users (id, name, email, hash, salt) VALUES (?, ?, ?, ?, ?)`;
+    this.db.run(sql, [ulid(), name, email, hash, salt], cb);
+  }
 }
 
-
-module.exports = (dbConn) =>{
-    return new UserDAO(dbConn);
-}
+module.exports = (dbConn) => {
+  return new UserDAO(dbConn);
+};
 
 // se os métodos forem estáticos
 // class UserDAO{
@@ -30,4 +50,3 @@ module.exports = (dbConn) =>{
 // }
 
 // module.exports = UserDAO;
-
